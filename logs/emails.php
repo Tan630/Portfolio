68c14493b41ddd634e9444d384fe1b81df3673e5
Yiding Li <yidingli1@outlook.com>
2023-04-15 20:56:50 +0800

    Implement email verification

--- logs/emails.php
+++
@@ -1,4 +0,0 @@
-2023-04-15, 10:13:14 | me@anonymous.com wrote: Hello there
-2023-04-15, 10:13:27 | what@message.com wrote: Please let me out
-2023-04-15, 10:13:50 | welcome@function.net wrote: Testing
-2023-04-15, 10:14:09 | spam@gorillamail.rocks wrote: I am junkmail
--- services/submit.php
+++ services/submit.php
@@ -1,6 +1,9 @@
 <?php
 define('ROOTPATH', __DIR__."/..");
 
+
+
+
 function logEmail(string $user = "Anonymous", string $content = ""): void {
     $time = gmdate("Y-m-d, H:i:s");
     file_put_contents(
@@ -10,9 +13,20 @@ function logEmail(string $user = "Anonymous", string $content = ""): void {
     );
 }
 
+
+
+function isValidUserName(string $mif): bool {
+    return (filter_var("$mif", FILTER_VALIDATE_EMAIL));
+}
+
+function isValidContent(string $mif): bool {
+    return ($mif == "");
+}
+
 # --- Output Below ---
 $user = $_REQUEST["cemail"];
 $message = $_REQUEST["ccontent"];
+if (isValidUserName($user) and isValidContent($message));
 logEmail((is_null($user)) ? "Anonymous":$user 
        , (is_null($message)) ? "Nothing":$message);
 #logEmail(implode(array_keys($_REQUEST)), !is_null($ccontent) ?: 'nothing');
