d03852234a8a0076274dc486b6ebb9e676b64438
Yiding Li <yidingli1@outlook.com>
2023-04-15 21:17:05 +0800

    Improve error feedbacck

--- services/submit.php
+++ services/submit.php
@@ -1,8 +1,7 @@
 <?php
+# ini_set('display_errors', 0);
+
 define('ROOTPATH', __DIR__."/..");
-
-
-
 
 function logEmail(string $user = "Anonymous", string $content = ""): void {
     $time = gmdate("Y-m-d, H:i:s");
@@ -13,22 +12,31 @@ function logEmail(string $user = "Anonymous", string $content = ""): void {
     );
 }
 
-
-
-function isValidUserName(string $mif): bool {
+function isValidUserName(?string $mif): bool {
     return (filter_var("$mif", FILTER_VALIDATE_EMAIL));
 }
 
-function isValidContent(string $mif): bool {
-    return ($mif == "");
+function isValidContent(?string $mif): bool {
+    return ($mif != "");
 }
 
 # --- Output Below ---
 $user = $_REQUEST["cemail"];
 $message = $_REQUEST["ccontent"];
-if (isValidUserName($user) and isValidContent($message));
-logEmail((is_null($user)) ? "Anonymous":$user 
-       , (is_null($message)) ? "Nothing":$message);
+$faviconPath = ROOTPATH . "/media/favicon.svg";
+if (isValidUserName($user) and isValidContent($message)) {
+    logEmail((is_null($user)) ? "Anonymous":$user
+    , (is_null($message)) ? "Nothing":$message);
+    echo "Request Successful!";
+} else {
+    http_response_code(500);
+    echo "Malformed input detected";
+}
+
+
 #logEmail(implode(array_keys($_REQUEST)), !is_null($ccontent) ?: 'nothing');
-    #!is_null($user) ?: "Anonymous", !is_null($ccontent) ?: 'nothing');
-?>
+#!is_null($user) ?: "Anonymous", !is_null($ccontent) ?: 'nothing');
+
+
+
+
